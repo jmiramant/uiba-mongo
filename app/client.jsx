2c80d0d69d1c30bf1016752412c2357826ff9446
jmiramant <josh@miramant.me>
2016-08-05 15:11:48 -0400

    adjusted heroku settings

--- app/client.jsx
+++ app/client.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+wimport React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
