61eb3a07100ed30b831e3fc0702f0056e25fe3b7
jmiramant <josh@miramant.me>
2016-08-21 17:01:04 -0600

    updated about text

--- app/containers/About.jsx
+++ app/containers/About.jsx
@@ -8,85 +8,23 @@ import xkcd from '../images/extrapolating.png';
 
 const cx = classNames.bind(styles);
 
-let date = [
-  'Around 15000 BCE hunter-gathers roam the world.',
-  'Around 14999 BCE the first career decision is made without data.',
-  'Around 10000 BCE Agriculture is invented.',
-  'Around 3000 BCE the first king shows up.',
-  'Around 500 BCE the division of labor happens.',
-  'Around 499 BC the first instance of career anxiety occurs.',
-  'Around 1700 the industrial revolution happens.',
-  'Around 1701 the first skills shortage occurs.',
-  'Around 1946 the first computer is built.',
-  'Around 1946 the first bug is found.',
-  'Around 2015 two friends get together and build the world’s finest machine learning platform for career planning.',
-  'Around 2016 the first company uses Uiba to increase productivity.',
-  'Around 2016 the first person uses Uiba to build a career map and take control of their future.',
-  '2017 and beyond'
-]
-
-let statement = [
-  "Woo Hoo! I’m the first entrepreneur!",
-  "Pfft I’ve had it with gathering! Hunting is where it’s at!",
-  "Woo hoo! Bread!",
-  "Thanks for the bread!",
-  "OMFG! Career choices!",
-  "So many choices, what’s my path?!",
-  "I need to hire workers!",
-  "What do you mean I have to pay them?!",
-  "I, for one, welcome our new computer overlords.",
-  "I, for one, welcome our new insect overlords.",
-  "Suck it, career anxiety!",
-  "Hey you did it! This is amazing! So listen, we have this bug…",
-  "I want to be a hunter-gatherer!",
-  "Bread for everyone!"
-]
-
-let timeline = date.map( (title, i) => {
-    return (
-      <StickyContainer key={1111 + i} style={{zIndex: 2}}>
-        <Sticky>
-          <h3 className={cx('aboutTime--title')}>{title}</h3>
-        </Sticky>
-          { i%2 === 0 ? (
-            <div className={cx('aboutTime--item')}>
-              <div className={cx('aboutTime--column-left')}>
-                <img src={xkcd} />
-              </div>
-              <p className={cx('aboutTime--column-right')} >{statement[i]}</p>
-            </div>
-          ) : (
-            <div className={cx('aboutTime--item')}>
-              <p className={cx('aboutTime--column-right')} >{statement[i]}</p>
-              <div className={cx('aboutTime--column-left')}>
-                <img src={xkcd} />
-              </div>
-            </div>
-          )}
-      </StickyContainer>
-    )
-  }
-)
-
 const About = () => {
   return (
     <div className={cx('about' + ' container')}>
-      <StickyContainer>
-        <Grid style={{marginTop: 40}}>
-          <div className={cx('row-flex', 'row-flex-wrap')}>
-            <Col xs={12} md={2} xsHidden smHidden className={cx('flex-col', 'timeline--column')} >
-              <StickyContainer style={{height: '100%'}}>
-                <Sticky>
-                  <h3>About Uiba</h3>
-                </Sticky>
-              </StickyContainer>
-            </Col>
-            <Col xs={12} md={10} xsHidden className={cx('flex-col', 'timeline--column')}>
-              { timeline }
-            </Col>
-          </div>
-        </Grid>
-      </StickyContainer>
+      <h2>Our Story</h2>
+      <p>The idea of Uiba began by listening to those around us. </p>
+      <p>Our friends and family, colleagues and acquaintances all struggled at times during their careers. For some it was trying to figure out what to do after graduation, for others it was realizing the career path they'd chosen wasn't right for them, for many it was simply wanting a new challenge or to do something more meaningful. </p>
+      <p>Whatever the person's background or experience, the process was always fraught with anxiety and frustration. This was usually caused by a lack of knowledge about the opportunities available (The world is a big place, how do I find something that fits me?) and a lack of understanding of how to get from here to there (What's my path?). We were sympathetic because we had also experienced these same frustrations. </p>
+      <p>So we sat down and started to think about this problem. And think. And think some more. Then we started talking to people in all walks of life from all over the planet (the internet is fantastic!) about this problem and how various people were dealing with it or trying to solve it.</p>
+      <p>What we realized was it's a problem of information and it affects everyone: </p>
+      <p>There's no one place I can go and learn about every possible career path and understand all the options, all the possibilities tailored specifically to me.</p>
+      <p>There's no one place I can go and understand how qualified I am for a given career path and find recommendations about what I need to develop and learn to achieve my goals.</p>
+      <p>There's no one place I can go to learn from the wisdom of other that have traveled career paths of interest to me or where I can share my own insights about my path with others.</p>
+      <p>There's no one place I can go and figure out how I get from where I am today to where I want to be tomorrow.</p>
+      <p>There's no one place I can go to do all of this. </p>
+      <p>But there should be, so that's what we're building. </p>
+      <p>We're building it because in this day and age there's no excuse for people suffering this much career anxiety and frustration or being trapped in jobs they dislike when there's so much each of us has to offer and contribute. So let's fix this for everyone.</p>
+      <p>We invite you to join us because we'd like to make that journey with you, together.</p>
     </div>
   );
 };
