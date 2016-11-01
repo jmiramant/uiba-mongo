bb0afcc8c5a408fb8b3e10ab8b94b5af714f3945
Josh Miramant <josh@miramant.me>
2016-10-31 21:40:17 -0600

    Removed keys

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,28 +1,33 @@
-export const sessionSecret = process.env.SESSION_SECRET || '6cKCWD75gHgzCvM4VQyR5_TU';
+export const sessionSecret = process.env.SESSION_SECRET || '<SESSION_SECRET>';
 
 export const google = {
-  clientID: process.env.GOOGLE_CLIENTID || '114596459676-b30kskdonpjh9069evk4tk62ou8pis4g.apps.googleusercontent.com',
-  clientSecret: process.env.GOOGLE_SECRET || '0Jg0XDNXlmM2UeDH6dXk8u-c',
-  callbackURL: process.env.GOOGLE_CALLBACK || '/auth/google/callback'
+  clientID: process.env.GOOGLE_CLIENTID || '<GOOGLE_CLIENTID>',
+  clientSecret: process.env.GOOGLE_SECRET || '<GOOGLE_SECRET>',
+  callbackURL: process.env.GOOGLE_CALLBACK || '<GOOGLE_CALLBACK>k'
 };
 
 export const linkedin = {
-  clientID: process.env.LINKEDIN_CLIENTID || '7502url4wogimg',
-  clientSecret: process.env.LINKEDIN_SECRET || 'Cbt1RZqeunrzRXP3',
-  callbackURL: process.env.GOOGLE_CALLBACK || '/auth/linkedin/callback'
+  clientID: process.env.LINKEDIN_CLIENTID || '<LINKEDIN_CLIENTID>',
+  clientSecret: process.env.LINKEDIN_SECRET || '<LINKEDIN_SECRET>',
+  callbackURL: process.env.GOOGLE_CALLBACK || '<GOOGLE_CALLBACK>'
 };
 
 export const sendInBlue = {
-  clientToken: process.env.SENDBLUE_TOKEN || 'MaNzWRXC0ELdyOQ1'
+  clientToken: process.env.SENDBLUE_TOKEN || '<SENDBLUE_TOKEN>'
 }
 
 export const externalAPISecret = {
-  token: process.env.UIBA_API_SECRET || 'MvUHj6PG6UPfeA2CccMBwTFn',
-  accessEmail: process.env.UIBA_API_SECRET || 'token@token.com'
+  token: process.env.UIBA_API_SECRET || '<UIBA_API_SECRET>',
+  accessEmail: process.env.UIBA_API_SECRET || '<UIBA_API_SECRET>m'
 }
 
 export const zipCodeAPI = {
-  key: process.env.zipCodeAPI_key || 'JCqjDuMQP0YpfmZglzDHV5yQI3faL0FvD5OhzB7pB6PRsO8OnLvjR49A7pLSqqn4',
+  key: process.env.zipCodeAPI_key || '<zipCodeAPI_key>',
+}
+
+export const aws = {
+  aws_access_key_id: process.env.AWS_ACCESS_KEY_ID || '<AWS_ACCESS_KEY_ID>',
+  aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY || '<AWS_SECRET_ACCESS_KEY>t'
 }
 
 export default {
