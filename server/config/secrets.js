29262d22228070b70b634490bec258793f438c98
jmiramant <josh@miramant.me>
2016-11-02 12:00:19 -0600

    token bug

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,39 +1,39 @@
-export const sessionSecret = process.env.SESSION_SECRET || '<SESSION_SECRET>';
+'use strict';
 
-export const google = {
-  clientID: process.env.GOOGLE_CLIENTID || '<GOOGLE_CLIENTID>',
-  clientSecret: process.env.GOOGLE_SECRET || '<GOOGLE_SECRET>',
-  callbackURL: process.env.GOOGLE_CALLBACK || '<GOOGLE_CALLBACK>k'
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var sessionSecret = exports.sessionSecret = process.env.SESSION_SECRET || '6cKCWD75gHgzCvM4VQyR5_TU';
+
+var google = exports.google = {
+  clientID: process.env.GOOGLE_CLIENTID || '114596459676-b30kskdonpjh9069evk4tk62ou8pis4g.apps.googleusercontent.com',
+  clientSecret: process.env.GOOGLE_SECRET || '0Jg0XDNXlmM2UeDH6dXk8u-c',
+  callbackURL: process.env.GOOGLE_CALLBACK || '/auth/google/callback'
 };
 
-export const linkedin = {
-  clientID: process.env.LINKEDIN_CLIENTID || '<LINKEDIN_CLIENTID>',
-  clientSecret: process.env.LINKEDIN_SECRET || '<LINKEDIN_SECRET>',
-  callbackURL: process.env.GOOGLE_CALLBACK || '<GOOGLE_CALLBACK>'
+var linkedin = exports.linkedin = {
+  clientID: process.env.LINKEDIN_CLIENTID || '7502url4wogimg',
+  clientSecret: process.env.LINKEDIN_SECRET || 'Cbt1RZqeunrzRXP3',
+  callbackURL: process.env.GOOGLE_CALLBACK || '/auth/linkedin/callback'
 };
 
-export const sendInBlue = {
-  clientToken: process.env.SENDBLUE_TOKEN || '<SENDBLUE_TOKEN>'
-}
+var sendInBlue = exports.sendInBlue = {
+  clientToken: process.env.SENDBLUE_TOKEN || 'MaNzWRXC0ELdyOQ1'
+};
 
-export const externalAPISecret = {
-  token: process.env.UIBA_API_SECRET || '<UIBA_API_SECRET>',
-  accessEmail: process.env.UIBA_API_SECRET || '<UIBA_API_SECRET>m'
-}
+var externalAPISecret = exports.externalAPISecret = {
+  token: process.env.UIBA_API_SECRET || 'MvUHj6PG6UPfeA2CccMBwTFn',
+  accessEmail: process.env.UIBA_API_SECRET || 'token@token.com'
+};
 
-export const zipCodeAPI = {
-  key: process.env.zipCodeAPI_key || '<zipCodeAPI_key>',
-}
+var zipCodeAPI = exports.zipCodeAPI = {
+  key: process.env.zipCodeAPI_key || 'JCqjDuMQP0YpfmZglzDHV5yQI3faL0FvD5OhzB7pB6PRsO8OnLvjR49A7pLSqqn4'
+};
 
-export const aws = {
-  aws_access_key_id: process.env.AWS_ACCESS_KEY_ID || '<AWS_ACCESS_KEY_ID>',
-  aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY || '<AWS_SECRET_ACCESS_KEY>t'
-}
-
-export default {
-  sessionSecret,
-  google,
-  externalAPISecret,
-  linkedin,
-  zipCodeAPI
+exports.default = {
+  sessionSecret: sessionSecret,
+  google: google,
+  externalAPISecret: externalAPISecret,
+  linkedin: linkedin,
+  zipCodeAPI: zipCodeAPI
 };
