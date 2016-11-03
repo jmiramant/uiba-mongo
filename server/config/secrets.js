ac36e475b10e4f8cf66fbb7b05d203ad53615829
Josh Miramant <josh@miramant.me>
2016-11-02 22:19:30 -0600

    Update Secerts

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,33 +1,33 @@
-export const sessionSecret = process.env.SESSION_SECRET || '<SESSION_SECRET>';
+export const sessionSecret = process.env.SESSION_SECRET
 
 export const google = {
-  clientID: process.env.GOOGLE_CLIENTID || '<GOOGLE_CLIENTID>',
-  clientSecret: process.env.GOOGLE_SECRET || '<GOOGLE_SECRET>',
-  callbackURL: process.env.GOOGLE_CALLBACK || '<GOOGLE_CALLBACK>k'
+  clientID: process.env.GOOGLE_CLIENTID,
+  clientSecret: process.env.GOOGLE_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK
 };
 
 export const linkedin = {
-  clientID: process.env.LINKEDIN_CLIENTID || '<LINKEDIN_CLIENTID>',
-  clientSecret: process.env.LINKEDIN_SECRET || '<LINKEDIN_SECRET>',
-  callbackURL: process.env.GOOGLE_CALLBACK || '<GOOGLE_CALLBACK>'
+  clientID: process.env.LINKEDIN_CLIENTID,
+  clientSecret: process.env.LINKEDIN_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK
 };
 
 export const sendInBlue = {
-  clientToken: process.env.SENDBLUE_TOKEN || '<SENDBLUE_TOKEN>'
+  clientToken: process.env.SENDBLUE_TOKEN
 }
 
 export const externalAPISecret = {
-  token: process.env.UIBA_API_SECRET || '<UIBA_API_SECRET>',
-  accessEmail: process.env.UIBA_API_SECRET || '<UIBA_API_SECRET>m'
+  token: process.env.UIBA_API_SECRET,
+  accessEmail: process.env.UIBA_API_SECRET
 }
 
 export const zipCodeAPI = {
-  key: process.env.zipCodeAPI_key || '<zipCodeAPI_key>',
+  key: process.env.zipCodeAPI_key,
 }
 
 export const aws = {
-  aws_access_key_id: process.env.AWS_ACCESS_KEY_ID || '<AWS_ACCESS_KEY_ID>',
-  aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY || '<AWS_SECRET_ACCESS_KEY>t'
+  aws_access_key_id: process.env.AWS_ACCESS_KEY_ID,
+  aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY
 }
 
 export default {
