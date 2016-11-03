22158928500bb204873dfaa08ebc7f4b16b20331
jmiramant <josh@miramant.me>
2016-11-03 11:29:24 -0600

    updated env nams

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,22 +1,22 @@
-export const sessionSecret = process.env.SESSION_SECRET || '6cKCWD75gHgzCvM4VQyR5_TU';
+export const sessionSecret = process.env.SESSION_SECRET;
 
 export const linkedin = {
-  clientID: process.env.LINKEDIN_CLIENTID || '7502url4wogimg',
-  clientSecret: process.env.LINKEDIN_SECRET || 'Cbt1RZqeunrzRXP3',
-  callbackURL: process.env.LINKEDIN_CALLBACK || '/auth/linkedin/callback'
+  clientID: process.env.LINKEDIN_CLIENTID,
+  clientSecret: process.env.LINKEDIN_SECRET,
+  callbackURL: process.env.LINKEDIN_CALLBACK
 };
 
 export const sendInBlue = {
-  clientToken: process.env.SENDBLUE_TOKEN || 'MaNzWRXC0ELdyOQ1'
+  clientToken: process.env.SENDBLUE_TOKEN
 }
 
 export const externalAPISecret = {
-  token: process.env.UIBA_API_SECRET || 'MvUHj6PG6UPfeA2CccMBwTFn',
-  accessEmail: process.env.UIBA_API_SECRET || 'token@token.com'
+  token: process.env.UIBA_API_SECRET,
+  accessEmail: process.env.UIBA_API_EMAIL
 }
 
 export const zipCodeAPI = {
-  key: process.env.zipCodeAPI_key || 'JCqjDuMQP0YpfmZglzDHV5yQI3faL0FvD5OhzB7pB6PRsO8OnLvjR49A7pLSqqn4',
+  key: process.env.zipCodeAPI_key,
 }
 
 export default {
