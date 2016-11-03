0f94fc9c36f7debce234867240a93bdb66d192b0
jmiramant <josh@miramant.me>
2016-11-03 11:37:38 -0600

    untracking

---
+++ server/config/secrets.js
@@ -0,0 +1,27 @@
+export const sessionSecret = process.env.SESSION_SECRET;
+
+export const linkedin = {
+  clientID: process.env.LINKEDIN_CLIENTID,
+  clientSecret: process.env.LINKEDIN_SECRET,
+  callbackURL: process.env.LINKEDIN_CALLBACK
+};
+
+export const sendInBlue = {
+  clientToken: process.env.SENDBLUE_TOKEN
+}
+
+export const externalAPISecret = {
+  token: process.env.UIBA_API_SECRET,
+  accessEmail: process.env.UIBA_API_EMAIL
+}
+
+export const zipCodeAPI = {
+  key: process.env.zipCodeAPI_key,
+}
+
+export default {
+  sessionSecret,
+  externalAPISecret,
+  linkedin,
+  zipCodeAPI
+};
