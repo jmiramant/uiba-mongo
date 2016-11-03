e13769baa995d41509fc588a7609317603972b42
jmiramant <josh@miramant.me>
2016-11-03 09:40:36 -0600

    updated token projection

--- server/db/controllers/tokens.js
+++ server/db/controllers/tokens.js
@@ -11,36 +11,36 @@ export function token(req, res) {
     if (req.headers.email.indexOf('@uiba.co') !== -1 || req.headers.email === externalAPISecret.accessEmail) {
       // Fetch the appropriate user, if they exist
       UserModel.findOne({ email: req.headers.email }, function(err, user) {
-        if (err || !user) {    
+        if (err || !user || !user.comparePassword) {    
           // user cannot be found; may wish to log that fact here. For simplicity, just return a 401
           res.send('Authentication error', 401)
+        } else {
+          return user.comparePassword(req.headers.password, (err, isMatch) => {
+            if (err) {
+              // an error has occured checking the password. For simplicity, just return a 401
+              res.send(err.response, 401)
+            }
+            if (isMatch) {  
+              // Great, user has successfully authenticated, so we can generate and send them a token.  
+              var expires = moment().add(7, 'days').valueOf()       
+
+              var token = jwt.encode(
+                {
+                  iss: user.id,
+                  exp: expires
+                }, 
+                externalAPISecret.token
+              );            
+              return res.json({
+                token : token,
+                expires : expires,
+                user : user.toJSON()
+              });
+            } else {            
+              res.send('Authentication error', 401)
+            }
+          });
         }
-        return user.comparePassword(req.headers.password, (err, isMatch) => {
-          if (err) {
-            // an error has occured checking the password. For simplicity, just return a 401
-            res.send(err.response, 401)
-          }
-          if (isMatch) {  
-            // Great, user has successfully authenticated, so we can generate and send them a token.  
-            var expires = moment().add(7, 'days').valueOf()       
-
-            var token = jwt.encode(
-              {
-                iss: user.id,
-                exp: expires
-              }, 
-              externalAPISecret.token
-            );            
-            return res.json({
-              token : token,
-              expires : expires,
-              user : user.toJSON()
-            });
-          } else {            
-            res.send('Authentication error', 401)
-          }
-        });
-
       });
     } else {
       res.send('Authentication error: Account not whitelisted.', 401)
