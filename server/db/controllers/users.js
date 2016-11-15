54e592034d49f5184a7d29c77e4a55549bee66de
jmiramant <josh@miramant.me>
2016-11-14 18:37:06 -0800

    updated user logs

--- server/db/controllers/users.js
+++ server/db/controllers/users.js
@@ -48,7 +48,7 @@ const logRecruiter = (req, profId) => {
 
 const resolveApplyRedirect = (req, profile, cb) => {
   const companyName = req.headers.referer.split('/apply/')[1].split('/')[0].split('?')[0];
-  const nameLower = companyName.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(' ').join('_')
+  const nameLower = companyName.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-`~()]/g,"").split(' ').join('_')
   Company.findOne({
     name_lower: nameLower
   }, (companyErr, _company) => {
