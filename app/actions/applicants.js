355a7993e32ea5fe255f2a02a9ffa33bf8f0fb11
jmiramant <josh@miramant.me>
2016-11-24 05:42:09 -0700

    update bundle

--- app/actions/applicants.js
+++ app/actions/applicants.js
@@ -9,25 +9,28 @@ function makeApplicantRequest(method, data, api = '/applicant') {
   return request[method](api, data);
 }
 
-export function fetchApplicants(id) {
-  return {
-    type: types.GET_APPLICANTS,
-    promise: makeApplicantRequest('get', {}, '/applicants/' + id)
-  };
-}
-
 export function fetchApplicant(id) {
   return {
     type: types.GET_APPLICANTS,
     promise: makeApplicantRequest('get', {}, '/applicants/' + id)
-  };
+  }
+}
+
+export function fetchApplicants(id) {
+  return {
+    type: types.GET_APPLICANTS,
+    promise: makeApplicantRequest('get', {}, '/applicants/' + id)
+  }
 }
 
 export function fetchApplicant(profId) {
   let path = '/profile';
-  if (profId) path = '/profile/' + profId;
+  if (profId) {
+    path = '/profile/' + profId;
+  }
+  
   return {
     type: types.GET_APPLICANT,
     promise: makeApplicantRequest('get', {}, path)
-  };
+  }
 }
