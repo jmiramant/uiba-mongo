b924686079d677253ceec176649efc9759752a3c
jmiramant <josh@miramant.me>
2016-12-02 10:52:25 -0700

    merged filtering to test

--- server/db/controllers/scores.js
+++ server/db/controllers/scores.js
@@ -13,13 +13,13 @@ export function post(req, res) {
     data["job_filter"] = JSON.parse(req.query.skills);
   }
 
+  // url: 'https://nisn4kwnfb.execute-api.us-east-1.amazonaws.com/multiends/score/v1/test' + process.env.NODE_ENV,
   request({
     url: 'https://nisn4kwnfb.execute-api.us-east-1.amazonaws.com/multiends/score/v1/test',
     json: JSON.parse(JSON.stringify(data).replace(/'/g, '"')),
     method: 'POST'
   }, (err, response, body) => {
     if (err) return res.status(404).send({err: err});
-    console.log(body['body-json'])
     return res.json(body['body-json']['results']);
   });
 }
