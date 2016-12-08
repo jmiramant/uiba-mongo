25c2c39a1cbc9731b2e9077e38e6a6505eb9dd28
jmiramant <josh@miramant.me>
2016-12-07 22:52:20 -0700

    updated w nr

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['uiba_web'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '6e102bc34ac1937cd66a08162b522fab13f5ae90',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
